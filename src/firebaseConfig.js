63829d683f6d8a1a0ca5875a954d098fe3fe8cd1
Emir Satıcı <info@ozcanaskilari.com>
2025-05-23 16:23:56 +0300

    firebase config düzeltildi

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDG9QiR_vKONq7bUncNEP9TTrWbJQwbqnY",
+  authDomain: "ozcan-aski.firebaseapp.com",
+  projectId: "ozcan-aski",
+  storageBucket: "ozcan-aski.firebasestorage.app",
+  messagingSenderId: "906176009967",
+  appId: "1:906176009967:web:33e1b56ca5b55bed112d1a"
 };
 
 const app = initializeApp(firebaseConfig);
